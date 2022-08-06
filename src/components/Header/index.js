f08026b65e36537d8b92139385c3638edcd45f9a
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-06 12:27:31 -0600

    created switch statement to auto load the about

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,29 @@
 // Will need to import all pages on Navbar (Nav, about, contact, portfolio, resume)
 import {useState} from 'react';
 import Nav from '../Nav';
+import About from '../About';
+import Contact from '../Contact';
+import Portfolio from '../Portfolio';
+import Resume from '../Resume';
 
 function Header() {
     const [currentPage, setCurrentPage] = useState('About');
+
+    // I'll need a switch statement to render the page that is clicked
+    const clickedPage = () => {
+        switch (currentPage) {
+            case 'About':
+                return <About />;
+            case 'Contact':
+                return <Contact />;
+            case 'Portfolio': 
+                return <Portfolio />;
+            case 'Resume':
+                return <Resume />;
+            default:
+                return <About />;
+        }
+    }
 
     return (
         <div>
