d83b27dee72ddd43440ec672be584c2409e313a5
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-06 15:02:58 -0600

    creating project components

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,67 +1,72 @@
 import React from 'react';
+import Project from '../Project';
 
 function Portfolio() {
     const projects = [
         {
             id: 1,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Bit Trail',
+            img: '../../assets/bit-trail.JPG',
+            liveSite: 'https://bit-trail.herokuapp.com/',
+            gitHub: 'https://github.com/CalebLawver/Bit-Trail'
         },
         {
             id: 2,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'National Mapper',
+            img: '../../assets/national-mapper.JPG',
+            liveSite: 'https://jacee94.github.io/national-mapper/',
+            gitHub: 'https://github.com/Jacee94/national-mapper'
         },
         {
             id: 3,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Final Project',
+            img: '../../assets/emp-trackert.JPG',
+            liveSite: '/',
+            gitHub: '/'
         },
         {
             id: 4,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Run Buddy',
+            img: '../../assets/run-buddy.JPG',
+            liveSite: 'https://caleblawver.github.io/run-buddy/',
+            gitHub: 'https://github.com/CalebLawver/run-buddy'
         },
         {
             id: 5,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Password Generator',
+            img: '../../assets/password-gen.JPG',
+            liveSite: 'https://caleblawver.github.io/Password-Generator/',
+            gitHub: 'https://github.com/CalebLawver/Password-Generator'
         },
         {
             id: 6,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Zoo Keepr',
+            img: '../../assets/zoo.JPG',
+            liveSite: 'https://frozen-bayou-44761.herokuapp.com/',
+            gitHub: 'https://github.com/CalebLawver/zookeepr'
         },
         {
             id: 7,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Budget PWA',
+            img: '../../assets/budget.JPG',
+            liveSite: 'https://mysterious-hollows-72686.herokuapp.com/',
+            gitHub: 'https://github.com/CalebLawver/Budget-PWA'
         },
         {
             id: 8,
-            title: '',
-            img: '',
-            liveSite: '',
-            gitHub: ''
+            title: 'Note Taker',
+            img: '../../assets/note.JPG',
+            liveSite: 'https://dry-escarpment-02911.herokuapp.com/',
+            gitHub: 'https://github.com/CalebLawver/note-taker'
         }
     ]
 
     return (
-        <p>Test</p>
+        <section className='container p-4'>
+            <h1 className='p-4 text-center'>My Projects</h1>
+            <hr />
+            <Project projects={projects}/>
+        </section>
     )
 }
 
---
+++ src/components/Project/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Project(props) {
+    return (
+        <div>
+            {/* Div for projects to live in */}
+            <div>
+                {/* div for individual projects to be mapped over and stored in */}
+                <div>
+                    {/* image of project */}
+                </div>
+                <div>
+                    {/* github link */}
+                </div>
+                <div>
+                    {/* live link */}
+                </div>
+            </div>
+        </div>
+    )
+}
