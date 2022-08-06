722849dfc07c2114a88f102f7738c1ba7a935efc
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-06 13:40:46 -0600

    About page created

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function About() {
     return (
-        <p>Test</p>
+        <div>
+            <p className="">Full Stack Developer in Training</p>
+            <hr/>
+            <div className="container">
+                <div className='row'>
+                    <img className="img-responsive rounded-circle col-4" src={require("../../assets/me.jpg")} alt="Caleb Lawver"/>
+            <p className='col-8'>
+                I am Caleb, I am a beginner Full Stack Developer with a lot of ambition to continue learning. I've just completed a six month bootcamp through the University of Utah that has taught me a great deal. 
+                I'm super excited to continue learning and growing my newfound skills. 
+            </p>
+                </div>
+                
+            </div>
+            
+        </div>
     )
 }
 
