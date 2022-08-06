1dc9716c3a09bdfb2a063e5b3394f12737014231
Richard Caleb Lawver <clawver30@gmail.com>
2022-08-06 14:40:40 -0600

    created array for projects to live in

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,65 @@
 import React from 'react';
 
 function Portfolio() {
+    const projects = [
+        {
+            id: 1,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 2,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 3,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 4,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 5,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 6,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 7,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        },
+        {
+            id: 8,
+            title: '',
+            img: '',
+            liveSite: '',
+            gitHub: ''
+        }
+    ]
+
     return (
         <p>Test</p>
     )
